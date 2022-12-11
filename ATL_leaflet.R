479174e3ea2651393c8fc18784e17bfd160d83c7
CuizhuoLu <82602259+CuizhuoLu@users.noreply.github.com>
2022-12-11 15:35:31 -0600

    Add files via upload

--- ATL_leaflet.R
+++ ATL_leaflet.R
@@ -13,9 +13,7 @@ npu<-pull(Atlanta,npu)%>%
   unique()%>%
   na.omit()
 
-
 pal <- colorFactor(c("blue","yellow","red"), domain = Atlanta$crime)
-ai_colors <-c("blue","yellow","red")
 crime_plot<-function(dt){
   sub<-dt%>%filter(selected)
     leaflet(sub) %>%
@@ -27,8 +25,7 @@ crime_plot<-function(dt){
       stroke = FALSE,
       fillOpacity = 1,
       popup = sub$crime,
-      fillColor = ~pal(sub$crime))%>%
-      addLegend( position = "topright", title="Crime", labels = unique(sub$crime))
+      fillColor = ~pal(sub$crime))
 }
 
 count_plot<-function(df){
@@ -37,11 +34,10 @@ count_plot<-function(df){
     count() %>%
     ggplot()+
     geom_line(aes(year,n,group=neighborhood,col=neighborhood),size=2) +
-    scale_x_continuous(limits = c(2009,2016), expand = c(0, 0))+
     scale_x_discrete(labels = year) +
     theme(text = element_text(size = 14, face = "bold")) +
     labs(title = "Crime amount of neighborhood")+
-    scale_x_continuous(limits = c(2010,2016), expand = c(0, 0))+
+    scale_x_continuous(limits = c(2009,2016), expand = c(0, 0))+
     xlab("Year") + ylab("Total Amount of Crimes")
 }
 ui<-fluidPage(
@@ -59,8 +55,6 @@ ui<-fluidPage(
       )
     )
   )
-  
-
 server<-function(input,output){
   output$selection1 <- renderUI({
     selectInput("neighborhood", "Select a neighborhood", 
@@ -71,20 +65,15 @@ server<-function(input,output){
                 choices = Atlanta[Atlanta$npu==input$NPU & Atlanta$neighborhood==input$neighborhood,"crime"],multiple=T)
   })
   crime_subset<-reactive({
-    Atlanta%>%mutate(selected=(crime%in%input$crime_type)&
-                       (npu%in%input$NPU)&
-                       (neighborhood%in%input$neighborhood))
+    Atlanta%>%mutate(selected= (npu%in%input$NPU)&
+                       (neighborhood%in%input$neighborhood)&
+                       (crime%in%input$crime_type))
   })
-  
-  
   output$plot1<-renderLeaflet({
     crime_plot(crime_subset())
   })
   output$plot2<-renderPlot({
     count_plot(crime_subset())
   })
-  
 }
-
-
 shinyApp(ui, server)
