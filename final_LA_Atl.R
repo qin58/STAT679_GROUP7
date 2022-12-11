3518ffa83b40d40394a20a58e8e71822fc384bab
CuizhuoLu <82602259+CuizhuoLu@users.noreply.github.com>
2022-12-11 15:43:19 -0600

    Add files via upload

---
+++ final_LA_Atl.R
@@ -0,0 +1,160 @@
+library(shiny)
+library(leaflet)
+library(tidyverse)
+library(data.table)
+library(shinythemes)
+library(plotly)
+library(lubridate)
+library(bslib)
+library(ggplot2)
+set.seed(100)
+crimeLA <- read.csv("crimeLA.csv")
+crimeLA = na.omit(crimeLA)
+crimeLA=sample_n(crimeLA,10000)
+
+crimeLA$Date.Reported <- as.POSIXct(paste(crimeLA$Date.Reported), format = "%m/%d/%Y")
+crimeLA$year = year(crimeLA$Date.Reported)
+crimeLA$year = as.numeric(as.character(crimeLA$year))
+crimeLA$Victim.Age = as.numeric(crimeLA$Victim.Age)
+
+Atlanta<-read.csv("https://query.data.world/s/eiblgqhd555clewhca3cpkgpq5jnf5")
+Atlanta$date <- as.POSIXct(paste(Atlanta$date), format = "%m/%d/%Y")
+Atlanta$year = year(Atlanta$date)
+Atlanta$year = as.numeric(as.character(Atlanta$year))
+
+npu<-pull(Atlanta,npu)%>%
+  unique()%>%
+  na.omit()
+pal <- colorFactor(c("blue","yellow","red"), domain = Atlanta$crime)
+crime_plot<-function(dt){
+  sub<-dt%>%filter(selected)
+  leaflet(sub) %>%
+    addProviderTiles(providers$CartoDB.Positron,group = "OSM") %>%
+    addCircleMarkers(
+      lng = sub$long,
+      lat = sub$lat,
+      radius = 4,
+      stroke = FALSE,
+      fillOpacity = 1,
+      popup = sub$crime,
+      fillColor = ~pal(sub$crime))
+}
+
+count_plot<-function(df){
+  df%>%filter(selected)%>%
+    group_by(year,neighborhood) %>%
+    count() %>%
+    ggplot()+
+    geom_line(aes(year,n,group=neighborhood,col=neighborhood),size=2) +
+    scale_x_discrete(labels = year) +
+    theme(text = element_text(size = 14, face = "bold")) +
+    labs(title = "Crime amount of neighborhood")+
+    scale_x_continuous(limits = c(2009,2016), expand = c(0, 0))+
+    xlab("Year") + ylab("Total Amount of Crimes")
+}
+
+#crimeLA$Victim.Descent = replace(crimeLA$Victim.Descent, c("O","","X","K","A","C","F","P","I","U","L"), "Other")
+#crimeLA$Victim.Descent[crimeLA$Victim.Descent == c("O","","X","K","A","C","F","P","I","U","L")] <- "Other"
+
+ui <- 
+  navbarPage("Past Crime of Neighborhoods",
+             tabPanel("LA Crime",
+                      
+                      leafletOutput("map",width="1100",height="900"),
+                      
+                      absolutePanel(class = "panel panel-default", fixed = TRUE,
+                                    draggable = TRUE, top = 60, left = "auto", right = 50, bottom = "auto",
+                                    width = 400, height = "auto", 
+                                    style = "margin-left: auto; 
+                margin-right: auto;
+                border-radius: 5pt; 
+                text-align: center;",
+                                    fluidRow(align="center",
+                                             h2("Crime"),
+                                             
+                                             selectizeInput("city", "Neighborhood",unique(crimeLA$Area.Name), multiple = TRUE, options = list(maxItems = 3)),
+                                             sliderInput("age", "Victim Age", min = min(crimeLA$Victim.Age), max =max(crimeLA$Victim.Age), c(10,98),sep=""),
+                                             
+                                             
+                                             plotlyOutput("line",width=350,height=300)))),
+             
+             tabPanel("Atlanta Crime",
+                      theme = bs_theme(bootswatch = "simplex"),
+                      tags$h1("Mapping Atlanta crime incidences from 2010 to 2016"),
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("NPU","Select a NPU",npu,multiple = T),
+                          uiOutput("selection1"),
+                          uiOutput("selection2")
+                        ),
+                        mainPanel(
+                          leafletOutput("plot1"),
+                          plotOutput("plot2")
+                        )
+                      )
+                      )
+             
+             
+  )
+
+
+server <- function(input, output) {
+  crime_subset<-reactive({
+    crimeLA %>%
+      filter((crimeLA$Area.Name %in% input$city),
+             (Victim.Age >= input$age[1]),
+             (Victim.Age <= input$age[2]))
+    
+  })
+  output$selection1 <- renderUI({
+    selectInput("neighborhood", "Select a neighborhood", 
+                choices = Atlanta[Atlanta$npu==input$NPU,"neighborhood"],multiple = T)
+  })
+  output$selection2 <- renderUI({
+    selectInput("crime_type", "Select a crime type", 
+                choices = Atlanta[Atlanta$npu==input$NPU & Atlanta$neighborhood==input$neighborhood,"crime"],multiple=T)
+  })
+  crime_subset_Atl<-reactive({
+    Atlanta%>%mutate(selected= (npu%in%input$NPU)&
+                       (neighborhood%in%input$neighborhood)&
+                       (crime%in%input$crime_type))
+  })
+  output$plot1<-renderLeaflet({
+    crime_plot(crime_subset_Atl())
+  })
+  output$plot2<-renderPlot({
+    count_plot(crime_subset_Atl())
+  })
+  
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      setView(
+        lng = -118.243683,
+        lat = 34.052235 ,
+        zoom = 10
+      ) %>%
+      addMarkers(
+        data=crime_subset(),
+        lng = ~longitude,
+        lat = ~latitude,
+        popup = ~paste("Date:",Date.Reported, "<br>", "Description:",Crime.Code.Description,"<br>", "Ethnicity:",Victim.Descent),
+        clusterOptions = markerClusterOptions())
+  })
+  
+  output$line <- renderPlotly({
+    ggplotly(crime_subset()%>%
+               group_by(year,Area.Name) %>%
+               count() %>%
+               ggplot()+
+               geom_bar(aes(year,n,fill=Area.Name),position="stack",stat="identity") +
+               xlab("Year") + ylab("Total Amount of Crimes") +
+               theme(legend.position='none'))%>%
+      config(displayModeBar=F)
+  })
+  
+}
+
+
+shinyApp(ui, server)
+
